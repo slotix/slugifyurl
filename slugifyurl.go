b8bd3d4828d2a19cbc07b2341f4b0ffc91db00be
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2016-09-01 22:25:54 +0200

    misspell

--- slugifyurl.go
+++ slugifyurl.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-//Options structure for slugification parameters initialization 
+//Options structure for slugification parameters initialization
 type Options struct {
 	SlashChar    string //Used to replace slashes
 	MaxLength    int    //Output string length limit
@@ -30,31 +30,31 @@ Examples
 */
 func Slugify(rawurl string, options Options) string {
 	sanitized := rawurl
-    //if true remove scheme information from result
+	//if true remove scheme information from result
 	if options.SkipScheme {
 		re := regexp.MustCompile(`^[\w]+://`)
 		sanitized = re.ReplaceAllString(sanitized, "")
 	}
-    //if true remove username and password information from result
+	//if true remove username and password information from result
 	if options.SkipUserinfo {
 		re := regexp.MustCompile(`[\w\-_\.]+:[^@]+?@`)
 		sanitized = re.ReplaceAllString(sanitized, "")
 	}
-    //Replace all slashes with SlashChar
+	//Replace all slashes with SlashChar
 	re := regexp.MustCompile(`[/]`)
 	sanitized = re.ReplaceAllString(sanitized, options.SlashChar)
 	// replace invalid chars for Windows file names with slashChar
 	if !options.UnixOnly {
 		//according to MS specs at https://msdn.microsoft.com/en-us/library/aa365247(VS.85)
-        //<>:\"/\\|?* These characters are not eligable for usage in Windows file names
+		//<>:\"/\\|?* These characters are not eligible for usage in Windows file names
 		re := regexp.MustCompile(`[<>:\"/\\|?*]`)
 		sanitized = re.ReplaceAllString(sanitized, options.SlashChar)
 	}
 	if len(options.SlashChar) > 0 {
-        // replace multiple slashChars with just one
+		// replace multiple slashChars with just one
 		re := regexp.MustCompile(fmt.Sprintf(`['\%s']{2,}`, options.SlashChar))
 		sanitized = re.ReplaceAllString(sanitized, options.SlashChar)
-        //remove trailing slashChars
+		//remove trailing slashChars
 		re = regexp.MustCompile(fmt.Sprintf(`['\%s']*$`, options.SlashChar))
 		sanitized = re.ReplaceAllString(sanitized, "")
 	}
